2d9c2dd083fc61cd7b82b2d2cd3f5a1d4d6c0a3c
azu <azuciao@gmail.com>
2014-03-02 19:27:27 +0900

    feat(travis): add gh-pages build script

---
+++ _travis/deploy-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+    echo "This is a pull request. No deployment will be done.";
+    exit 0;
+fi
+
+asciidoctor -a icons=font -a source-highlighter=pygments --backend html5 -o index.html index.adoc
+(
+ lastCommit=$(git log --oneline | head -n 1)
+ echo "=COMMIT="
+ echo "MESSAGE :" $lastCommit
+
+ git checkout -B gh-pages
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add -A .
+ git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+ rm index.html
+)
