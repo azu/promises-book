d011230e76330184471b20dd0bbbe7e349920375
azu <azuciao@gmail.com>
2014-03-10 00:20:13 +0900

    feat(Makefile): auto generate embed code.

--- Ch1_WhatsPromises/src/promise-workflow.js
+++ Ch1_WhatsPromises/src/promise-workflow.js
@@ -1,16 +1,17 @@
 "use strict";
 function asyncFunction() {
-    return new Promise(function (resolve, reject) { // <1>
+    // <1>
+    return new Promise(function (resolve, reject) {
         setTimeout(function () {
             resolve('Async Hello world');
         }, 16);
     });
 }
-asyncFunction() // <2>
-    .then(function (value) {
-        console.log(value); // => 'Async Hello world'
-    }).catch(function (error) {
-        console.log(error);
-    });
+// <2>
+asyncFunction().then(function (value) {
+    console.log(value); // => 'Async Hello world'
+}).catch(function (error) {
+    console.log(error);
+});
 
 module.exports = asyncFunction;
--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ all: clean html
 
 html:
 	@echo "Generate HTML..."
+	@gulp embed
 	@asciidoctor -a icons=font -a source-highlighter=coderay --backend html5 -o ${OUTPUT_FILE} ${SRC_FILE}
 	@echo "Done! => ${OUTPUT_FILE}"
 
