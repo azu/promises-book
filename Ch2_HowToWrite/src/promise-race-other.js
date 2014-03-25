0727a7e07c8bf3938f3f7f70f4a4f4d8002f58c2
azu <azuciao@gmail.com>
2014-03-25 19:35:22 +0900

    write(Promise.race): `Promise.race` による処理のレースコンディションについて
    Promisesでは必ず処理は実行されることについての説明

---
+++ Ch2_HowToWrite/src/promise-race-other.js
@@ -0,0 +1,18 @@
+"use strict";
+var winnerPromise = new Promise(function (resolve) {
+    setTimeout(function () {
+        console.log("this is winner");
+        resolve("this is winner");
+    }, 4);
+});
+var loserPromise = new Promise(function (resolve) {
+    setTimeout(function () {
+        console.log("this is loser");
+        resolve("this is loser");
+    }, 1000);
+});
+
+// 一番最初のものがresolveされた時点で終了
+Promise.race([winnerPromise, loserPromise]).then(function (value) {
+    console.log(value); // => 1
+});
---
+++ Ch2_HowToWrite/test/promise-race-other-test.js
@@ -0,0 +1,32 @@
+/**
+ * Created by azu on 2014/03/25.
+ * LICENSE : MIT
+ */
+"use strict";
+var assert = require("power-assert");
+var sinon = require("sinon");
+var racer = require("../src/promise-race-other");
+describe("promise-race-other", function () {
+    it("should call both promise", function (done) {
+        var count = 0;
+        var winnerPromise = new Promise(function (resolve) {
+            setTimeout(function () {
+                count++;
+                resolve("this is winner");
+            }, 4);
+        });
+        var loserPromise = new Promise(function (resolve) {
+            setTimeout(function () {
+                count++;
+                resolve("this is loser");
+            }, 64);
+        }).then(function () {
+                assert(count === [winnerPromise, loserPromise].length);
+                done();
+            });
+
+        Promise.race([winnerPromise, loserPromise]).then(function (value) {
+            assert(count === [winnerPromise, loserPromise].length);
+        });
+    });
+});
