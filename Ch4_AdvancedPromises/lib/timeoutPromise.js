07e5bf2489ae3bfbced74ceacb2192abc96d0d66
azu <azuciao@gmail.com>
2014-05-15 09:39:07 +0900

    refactor(timeout): Promise.rejectを使うようにした
    https://github.com/azu/promises-book/issues/33

--- Ch4_AdvancedPromises/lib/timeoutPromise.js
+++ Ch4_AdvancedPromises/lib/timeoutPromise.js
@@ -2,10 +2,8 @@
 var TimeoutError = require("./TimeoutError").TimeoutError;
 var delayPromise = require("./delayPromise").delayPromise;
 function timeoutPromise(promise, ms) {
-    var timeout = new Promise(function (resolve, reject) {
-        return delayPromise(ms).then(function () {
-            reject(new TimeoutError("Operation timed out after " + ms + " ms"));
-        })
+    var timeout = delayPromise(ms).then(function () {
+        return Promise.reject(new TimeoutError("Operation timed out after " + ms + " ms"));
     });
     return Promise.race([promise, timeout]);
 }
