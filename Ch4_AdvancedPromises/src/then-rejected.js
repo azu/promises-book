a0cac2d8a4f5a844852e0919452fea0944595e12
azu <azuciao@gmail.com>
2014-05-16 08:23:45 +0900

    write(not-throw-use-reject): セクションのアウトラインを書いた

--- Ch4_AdvancedPromises/src/then-rejected.js
+++ Ch4_AdvancedPromises/src/then-rejected.js
@@ -1,1 +1,8 @@
 "use strict";
+var promise = Promise.resolve();
+promise.then(function () {
+    var retPromise = new Promise(function (resolve, reject) {
+        // resolve or reject
+    });
+    return retPromise;// <1>
+}).then(onFulfilled, onRejected);
