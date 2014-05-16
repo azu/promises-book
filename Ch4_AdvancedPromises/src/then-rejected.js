62caa9375dfffa76be1e5fef48d8cbf346744c90
azu <azuciao@gmail.com>
2014-05-16 20:01:44 +0900

    write(not-throw-use-reject): なぜthrowではなくrejectを使ったほうがいいかについて

--- Ch4_AdvancedPromises/src/then-rejected.js
+++ Ch4_AdvancedPromises/src/then-rejected.js
@@ -1,8 +1,5 @@
 "use strict";
 var promise = Promise.resolve();
 promise.then(function () {
-    var retPromise = new Promise(function (resolve, reject) {
-        // resolve or reject
-    });
-    return retPromise;// <1>
-}).then(onFulfilled, onRejected);
+    return Promise.reject(new Error("this promise is rejected"));
+}).then(console.log.bind(console), console.error.bind(console));
