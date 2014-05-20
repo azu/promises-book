74996a00dfb80fef1ec7d633ea99ead6940e9a84
azu <azuciao@gmail.com>
2014-05-21 08:34:29 +0900

    write(promise-done): unhandled rejectionについてを追加
    NOTEに各ライブラリの実装について書いた

--- Ch4_AdvancedPromises/src/promise-done-example.js
+++ Ch4_AdvancedPromises/src/promise-done-example.js
@@ -1,14 +1,7 @@
 "use strict";
 var done = require("../lib/promise-prototype-done");
-var promiseT = Promise.resolve();
-promiseT.then(function () {
-    JSON.parse("this is not json");
-}).catch(function (error) {
-    console.error(error);// => "SyntaxError: JSON.parse: unexpected keyword at line 1 column 1 of the JSON data"
-});
-// vs
-var promiseD = Promise.resolve();
-promiseD.done(function () {
+var promise = Promise.resolve();
+promise.done(function () {
     JSON.parse('this is not json');
     // => SyntaxError: JSON.parse: unexpected keyword at line 1 column 1 of the JSON data
 });
