b7b7afd79bde6d964947432c654dd94b3ece1b44
azu <azuciao@gmail.com>
2014-05-21 22:41:23 +0900

    write(promise-done): doneの実装とまとめを追加

--- Ch4_AdvancedPromises/lib/promise-prototype-done.js
+++ Ch4_AdvancedPromises/lib/promise-prototype-done.js
@@ -1,9 +1,9 @@
 "use strict";
 if (typeof Promise.prototype.done === "undefined") {
     Promise.prototype.done = function (onFulfilled, onRejected) {
-        this.then(onFulfilled, onRejected).catch(function (err) {
+        this.then(onFulfilled, onRejected).catch(function (error) {
             setTimeout(function () {
-                throw err;
+                throw error;
             }, 0);
         });
     };
