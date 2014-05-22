320ed48234347373fd5dae73d966d240d00222c1
azu <azuciao@gmail.com>
2014-05-22 10:41:00 +0900

    chore(index): move inline script to file

---
+++ public/js/google-analytics.js
@@ -0,0 +1,19 @@
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-2184335-14', 'azu.github.io');
+ga('send', 'pageview');
+window.addEventListener("hashchange", function hashChange() {
+    ga('send', 'pageview', {
+        'page': location.pathname + location.search + location.hash
+    });
+}, false);
