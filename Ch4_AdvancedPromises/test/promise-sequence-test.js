c9889356de4db159bd2957915e7696b541459bec
azu <azuciao@gmail.com>
2014-05-23 07:53:25 +0900

    feat(promise-sequence): sequenceの実装

---
+++ Ch4_AdvancedPromises/test/promise-sequence-test.js
@@ -0,0 +1,21 @@
+"use strict";
+var assert = require("power-assert");
+var sequence = require("../lib/promise-sequence").sequencePromises;
+describe("promise-sequence", function () {
+    it("should sequence promises", function () {
+        var promisedIdentity = [1, 2, 4, 8, 16, 32].map(function (value) {
+            return function identify() {
+                return new Promise(function (resolve) {
+                    setTimeout(function () {
+                        resolve(value);
+                    }, value);
+                })
+            }
+        });
+        var startDate = Date.now();
+        return sequence(promisedIdentity).then(function (values) {
+            console.log(Date.now() - startDate + "ms");// 約64ms
+            assert.deepEqual(values, [1, 2, 4, 8, 16, 32]);
+        });
+    });
+});
