bec0be9017cbe721e2339169156bd2f86de72756
azu <azuciao@gmail.com>
2014-05-24 13:31:49 +0900

    write(promise-then): promise chainの流れについての解説

---
+++ Ch2_HowToWrite/src/promise-then-catch-flow.js
@@ -0,0 +1,20 @@
+"use strict";
+function taskA() {
+    console.log("Task A");
+}
+function taskB() {
+    console.log("Task B");
+}
+function onRejected(error) {
+    console.log("Catch Error: A or B", error);
+}
+function finalTask() {
+    console.log("Final Task");
+}
+
+var promise = Promise.resolve();
+promise
+    .then(taskA)
+    .then(taskB)
+    .catch(onRejected)
+    .then(finalTask);
