d2d962feb06436ce1f5e0b5d077530534566bc96
azu <azuciao@gmail.com>
2014-05-25 16:55:12 +0900

    fix(all): インラインコードがパース出来ないものを修正

---
+++ test/inline-script/run-inline-script-test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by azu on 2014/05/25.
+ * LICENSE : MIT
+ */
+"use strict";
+var checkInlineScript = require("./inline-script-tester").checkInlineScript;
+checkInlineScript("../../").catch(function (error) {
+//    console.log(error);
+});
