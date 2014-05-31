6abd4ae2b1b9b03c8f3b57019ef14c9b855f091e
azu <azuciao@gmail.com>
2014-05-31 18:48:17 +0900

    chore(travis): git merge

--- _tools/deploy-preview-html.sh
+++ _tools/deploy-preview-html.sh
@@ -7,6 +7,7 @@ git checkout -B preview-html
 make html
 cat ./index.html | _tools/cli-rel-to-abs.js | _tools/cli-inject-meta.js > $TRAVIS_JOB_NUMBER.html
 
+git pull
 git add -A $TRAVIS_JOB_NUMBER.html
 git commit -m "Preview HTML $TRAVIS_BUILD_NUMBER"
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" preview-html > /dev/null
