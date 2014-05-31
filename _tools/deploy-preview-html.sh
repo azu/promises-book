18f74c207ec87ffd1b8fcb3b78e3d73ad7f2ea65
azu <azuciao@gmail.com>
2014-05-31 18:50:48 +0900

    chore(travis): git push -f

--- _tools/deploy-preview-html.sh
+++ _tools/deploy-preview-html.sh
@@ -10,6 +10,6 @@ cat ./index.html | _tools/cli-rel-to-abs.js | _tools/cli-inject-meta.js > $TRAVI
 git pull
 git add -A $TRAVIS_JOB_NUMBER.html
 git commit -m "Preview HTML $TRAVIS_BUILD_NUMBER"
-git push --quiet "https://${GH_TOKEN}@${GH_REF}" preview-html > /dev/null
+git push --quiet --force "https://${GH_TOKEN}@${GH_REF}" preview-html > /dev/null
 
 curl -d message="http://htmlpreview.github.io/?https://github.com/azu/promises-book/blob/preview-html/$TRAVIS_JOB_NUMBER.html" https://webhooks.gitter.im/e/${GITTER_TOKEN}
