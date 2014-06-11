f8b1a6ded69370af214b76273a8e076198cc2465
azu <azuciao@gmail.com>
2014-06-12 07:49:34 +0900

    feat(console): コンソールエディタに`Promise`のpolyfill参照させるように
    ブラウザにPromiseが実装されてなくてもサンプルが実行できるようになる
    
    　
    fixes #124

--- public/js/editor.js
+++ public/js/editor.js
@@ -3,8 +3,12 @@
  * LICENSE : MIT
  */
 "use strict";
+var Promise = require("ypromise");
 module.exports.initilize = function () {
     var consoleUI = require("codemirror-console-ui");
+    consoleUI.setUserContext({
+        Promise: Promise
+    });
     var codeBlocks = document.querySelectorAll("div.listingblock");
     for (var i = 0; i < codeBlocks.length; i++) {
         var codeBlock = codeBlocks[i];
