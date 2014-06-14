043dd5780310687532520d70b6ecdc0de236ae05
azu <azuciao@gmail.com>
2014-06-14 16:32:46 +0900

    feat(Make): suppport Make pdf 

---
+++ _tools/build_pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare parentDir=$(cd $(dirname $(cd $(dirname $0);pwd));pwd)
+declare currentDir=$(cd $(dirname $0);pwd)
+SRC_FILE=${parentDir}/index.xml
+OUTPUT_FILE=${parentDir}/promises-book.pdf
+
+${currentDir}/fopub/fopub "${SRC_FILE}" \
+-param body.font.family VL-Gothic-Regular \
+-param dingbat.font.family VL-Gothic-Regular \
+-param monospace.font.family VL-Gothic-Regular \
+-param sans.font.family VL-Gothic-Regular \
+-param title.font.family VL-Gothic-Regular \
+-param alignment left
