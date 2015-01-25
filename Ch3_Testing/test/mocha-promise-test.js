65647bc7dc495cd4c903477e586e16a347b466fd
azu <azuciao@gmail.com>
2015-01-25 19:50:13 +0900

    chore(ch3): コード例を簡略化

--- Ch3_Testing/test/mocha-promise-test.js
+++ Ch3_Testing/test/mocha-promise-test.js
@@ -2,9 +2,9 @@
 var assert = require("power-assert");
 describe("Promise Test", function () {
     it("should return a promise object", function () {
-        var promise = Promise.resolve(1);
+        var promise = Promise.resolve(42);
         return promise.then(function (value) {
-            assert(value === 1);
+            assert(value === 42);
         });
     });
 });
