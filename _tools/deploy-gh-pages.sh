6328f118e6078ff2c62690d36abcaf5aae1da1c4
azu <azuciao@gmail.com>
2015-02-24 07:45:56 +0900

    fix(pdf): pdfがgh-pagesで無視されてるのを修正

--- _tools/deploy-gh-pages.sh
+++ _tools/deploy-gh-pages.sh
@@ -24,6 +24,7 @@ lastCommit=$(git log --oneline | head -n 1)
 echo "=COMMIT="
 echo "MESSAGE :" $lastCommit
 
-git add -A -f .
+git add -A .
+git add -f javascript-promise-book.pdf
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null
