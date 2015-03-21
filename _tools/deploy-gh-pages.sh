b082b96cb64b4d83c2dfad7d31724c0457f8ae91
azu <azuciao@gmail.com>
2015-03-21 19:11:47 +0900

    fix(travis): remove `--quiet` option

--- _tools/deploy-gh-pages.sh
+++ _tools/deploy-gh-pages.sh
@@ -22,6 +22,6 @@ lastCommit=$(git log --oneline | head -n 1)
 echo "=COMMIT="
 echo "MESSAGE :" $lastCommit
 
-git add --quiet -A .
+git add -A .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null
