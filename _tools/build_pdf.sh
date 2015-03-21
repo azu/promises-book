906a56cc9d40367c5570498deafb17e84dd7c1d6
azu <azuciao@gmail.com>
2015-03-21 18:57:07 +0900

    chore(pdf): fix pdf script

--- _tools/build_pdf.sh
+++ _tools/build_pdf.sh
@@ -11,6 +11,8 @@ cd ${currentDir}
 CUSTOM_CONFIG_PATH="${currentDir}/fopub-config/custom-config.xml"
 # WTF http://d.hatena.ne.jp/hkobayash/20111106/1320560290
 ESCAPE_CONFIG_PATH=`echo $CUSTOM_CONFIG_PATH | sed "s/\//\\\\\\\\\//g"`
+# hack rewrite fopub/fopub
+sed -i .bak "s/\$DOCBOOK_XSL_DIR\/fop-config.xml/$ESCAPE_CONFIG_PATH/" "${currentDir}/fopub/fopub"
 
 ${currentDir}/fopub/fopub "${SRC_FILE}" \
 -param body.font.family GenShinGothic-P-Regular \
@@ -19,3 +21,6 @@ ${currentDir}/fopub/fopub "${SRC_FILE}" \
 -param sans.font.family GenShinGothic-P-Regular \
 -param title.font.family GenShinGothic-P-Regular \
 -param alignment left
+
+# back .bak to fopub
+mv -f ${currentDir}/fopub/fopub.bak ${currentDir}/fopub/fopub
