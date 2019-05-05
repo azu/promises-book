83b3200d2ab255b7e6f518bff5a9b60d095ec4d4
azu <azuciao@gmail.com>
2019-05-05 22:53:01 +0900

    fix(polyfill): use core-js instead of native-promise-only

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,6 @@ gulp.task("embed", function() {
         .pipe(gulp.dest("./"));
 });
 gulp.task("lint-html", function(callback) {
-    require("native-promise-only");
     var File = require("./Ch4_AdvancedPromises/src/promise-chain/fs-promise-chain");
     var checkHTML = require("./test/html/missing-internal-link").checkInternalLinks;
 
