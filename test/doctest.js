42d40a4dffb1c467b2cab8a263e293466caa6628
azu <azuciao@gmail.com>
2019-09-01 20:30:43 +0900

    fix: tests

--- test/doctest.js
+++ test/doctest.js
@@ -3,9 +3,6 @@ const { parse } = require("@power-doctest/asciidoctor");
 const globby = require("globby");
 const fs = require("fs");
 const path = require("path");
-const transform = (code) => {
-    return code; // you need pre transform for the code if needed.
-};
 // = の段落が順番でないため do not support nested sections が発生する問題を回避する
 // ダミー文字列に書き換える
 const replaceDummyHeader = (content) => {
@@ -15,7 +12,6 @@ const replaceDummyHeader = (content) => {
         });
     }).join("\n");
 };
-// doctest for source/**/*.md
 describe("doctest:adoc", function () {
     const sourceDir = path.join(__dirname, "..", "Ch5_AsyncFunction");
     const files = globby.sync([
@@ -26,11 +22,9 @@ describe("doctest:adoc", function () {
         const normalizeFilePath = filePath.replace(sourceDir, "");
         describe(`${normalizeFilePath}`, function () {
             const content = fs.readFileSync(filePath, "utf-8");
-            let content1 = replaceDummyHeader(content);
-            console.log(content1);
             const parsedCodes = parse({
                 filePath,
-                content: content1
+                content: replaceDummyHeader(content)
             });
             console.log("parsedCodes", parsedCodes);
             // try to eval
