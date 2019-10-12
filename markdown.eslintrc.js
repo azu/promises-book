1b867110fd105d86f83ea31c6926f40c1dafe560
azu <azuciao@gmail.com>
2019-10-12 15:19:52 +0900

    refactor(doc): apply eslint to adoc inline code

---
+++ markdown.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "rules": {
+        "indent": "off",
+        "no-undef": "off",
+        "no-unused-vars": "off",
+        "semi": "off" // bug?
+    },
+    "extends": [
+        "./.eslintrc.js"
+    ]
+};
