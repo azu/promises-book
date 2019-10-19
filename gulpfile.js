b638d42a414c986ad811bcc13e3583da801770c0
azu <azuciao@gmail.com>
2019-10-19 16:46:56 +0900

    refactor(gulp): update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,53 @@
 "use strict";
-var gulp = require("gulp");
-var path = require("path");
-var concat = require('gulp-concat');
-var rename = require("gulp-rename");
-var inlining = require("gulp-inlining-node-require");
-var browserify = require('browserify');
-var minifyify = require("minifyify");
-var source = require('vinyl-source-stream');
-var sourceFile = "./public/js/index.js";
-var destCSSDir = "./public/css/build/";
-var destJSDir = "./public/js/build/";
-gulp.task("build-css", function() {
+const gulp = require("gulp");
+const path = require("path");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const inlining = require("gulp-inlining-node-require");
+const browserify = require("browserify");
+const minifyify = require("minifyify");
+const source = require("vinyl-source-stream");
+const sourceFile = "./public/js/index.js";
+const destCSSDir = "./public/css/build/";
+const destJSDir = "./public/js/build/";
+gulp.task("build-css", () => {
     return gulp.src([
         "./node_modules/codemirror/lib/codemirror.css",
         "./public/css/mirror-console-component.css",
         "./public/css/overload.css"
     ])
-        .pipe(concat('all.css'))
+        .pipe(concat("all.css"))
         .pipe(gulp.dest(destCSSDir));
 });
-gulp.task("build-js", function() {
+gulp.task("build-js", () => {
     return browserify(sourceFile)
         .bundle()
         .pipe(source("app.js"))
         .pipe(gulp.dest(destJSDir));
 });
-gulp.task("build-js-min", function() {
+gulp.task("build-js-min", () => {
     return browserify({
         debug: true
     })
         .add(sourceFile)
-        .plugin('minifyify', {
-            map: 'app.min.js.map',
-            output: destJSDir + 'app.min.js.map',
+        .plugin("minifyify", {
+            map: "app.min.js.map",
+            output: destJSDir + "app.min.js.map",
             compressPath: function(p) {
-                return path.relative('./', p);
+                return path.relative("./", p);
             }
         })
         .bundle()
         .pipe(source("app.js"))
         .pipe(gulp.dest(destJSDir));
 });
-gulp.task("embed", function() {
+gulp.task("embed", () => {
     const formatCode = require("./_tools/gulp/format-code");
-    return gulp.src(["./Ch*/src/**/*.js", "./Ch*/lib/*.js", "./Ch3_Testing/test/*.js"], { base: './' })
+    return gulp.src(["./Ch*/src/**/*.js", "./Ch*/lib/*.js", "./Ch3_Testing/test/*.js"], { base: "./" })
         .pipe(inlining())
         .pipe(formatCode())
-        .pipe(rename(function(filePath) {
-            var filePathBySplit = filePath.dirname.split(path.sep);
+        .pipe(rename((filePath) => {
+            const filePathBySplit = filePath.dirname.split(path.sep);
             filePathBySplit.pop();
             // src 以下の階層
             if (filePathBySplit[filePathBySplit.length - 1] === "src") {
@@ -58,25 +58,25 @@ gulp.task("embed", function() {
         }))
         .pipe(gulp.dest("./"));
 });
-gulp.task("lint-html", function(callback) {
-    var File = require("./Ch4_AdvancedPromises/src/promise-chain/fs-promise-chain");
-    var checkHTML = require("./test/html/missing-internal-link").checkInternalLinks;
+gulp.task("lint-html", (callback) => {
+    const File = require("./Ch4_AdvancedPromises/src/promise-chain/fs-promise-chain");
+    const checkHTML = require("./test/html/missing-internal-link").checkInternalLinks;
 
-    var htmlPromise = File.read("index.html").then(function(contents) {
-        var errors = checkHTML(contents);
+    const htmlPromise = File.read("index.html").then((contents) => {
+        const errors = checkHTML(contents);
         if (errors.length > 0) {
-            errors.forEach(function(error) {
+            errors.forEach((error) => {
                 console.error(error.message);
             });
             return callback(new Error("Found lint error"));
         }
         callback();
     });
-    var asciidocPromise = require("./test/inline-script/inline-script-tester")
+    const asciidocPromise = require("./test/inline-script/inline-script-tester")
         .checkInlineScript("./");
     Promise.all([htmlPromise, asciidocPromise])
         .catch(callback);
 });
-gulp.on('err', function(error) {
+gulp.on("err", () => {
     process.exit(1);
 });
