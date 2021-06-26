8dde632ab5824737c665d187fa2d394b2eaaa385
azu <azu@users.noreply.github.com>
2021-06-26 23:16:57 +0900

    fix(ch4): remove duplicated (#356)
    * fix(ch4): remove duplicated
    
    * chore(dep): update lock

--- Ch4_AdvancedPromises/src/race-delay-timeout/delay-race-cancel-play.js
+++ Ch4_AdvancedPromises/src/race-delay-timeout/delay-race-cancel-play.js
@@ -1,6 +1,12 @@
 "use strict";
-const timeoutPromise = require("../../lib/timeoutPromise").timeoutPromise;
 const TimeoutError = require("../../lib/TimeoutError").TimeoutError;
+const delayPromise = require("../../lib/delayPromise").delayPromise;
+function timeoutPromise(promise, ms) {
+    const timeout = delayPromise(ms).then(() => {
+        return Promise.reject(new TimeoutError("Operation timed out after " + ms + " ms"));
+    });
+    return Promise.race([promise, timeout]);
+}
 const cancelableXHR = require("./delay-race-cancel").cancelableXHR;
 const object = cancelableXHR("https://httpbin.org/get");
 // main
