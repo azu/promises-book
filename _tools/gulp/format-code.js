f0d672bd5657c6afbb0e989726f2862e203b611b
Noritaka Kobayashi <arukimedesu1166777@gmail.com>
2025-06-07 13:21:18 +0900

    refactor: through2 を使わず node の stream を使うようにした (#383)

--- _tools/gulp/format-code.js
+++ _tools/gulp/format-code.js
@@ -1,18 +1,21 @@
-var through2 = require('through2');
-var reg = /require\(["']power-assert["']\)/g;
+const { Transform } = require("stream");
+const reg = /require\(["']power-assert["']\)/g;
 
 function modifier(str) {
     // power-assert -> assert
     // strict use strict
-    return str.replace(reg, 'require("assert")').replace(/["']use strict["'];?\n/g, "")
+    return str.replace(reg, "require(\"assert\")").replace(/["']use strict["'];?\n/g, "");
 }
 
 function modify() {
-    return through2.obj(function (file, encoding, done) {
-        var content = modifier(String(file.contents));
-        file.contents = new Buffer(content);
-        this.push(file);
-        done();
+    return new Transform({
+        objectMode: true,
+        transform(file, encoding, callback) {
+            const content = modifier(String(file.contents));
+            file.contents = Buffer.from(content);
+            this.push(file);
+            callback();
+        }
     });
 }
 
